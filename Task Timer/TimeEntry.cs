e8432cbf052765c0986f7ae25016a77d07444661
Steffen Forkmann <steffen.forkmann@msu-solutions.de>
2010-07-16 15:45:54 +0200

    Cleaned TimeEntry.

--- Task Timer/TimeEntry.cs
+++ Task Timer/TimeEntry.cs
@@ -1,11 +1,8 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 
 namespace Task_Timer
 {
-    class TimeEntry
+    internal class TimeEntry
     {
         public DateTime Start { get; set; }
         public DateTime Stop { get; set; }
@@ -14,42 +11,25 @@ namespace Task_Timer
         {
             if (Stop.Year != 1)
                 return Stop.Ticks - Start.Ticks;
-            else
-                return DateTime.Now.Ticks - Start.Ticks;
+            return DateTime.Now.Ticks - Start.Ticks;
         }
 
         public long DurationInSeconds()
         {
-            long seconds = 0;
+            long seconds;
             if (Stop.Year != 1)
             {
-                seconds = (Stop.Ticks - Start.Ticks) / TimeSpan.TicksPerSecond;
-                if ((Stop.Ticks - Start.Ticks) % TimeSpan.TicksPerSecond > 0)
+                seconds = (Stop.Ticks - Start.Ticks)/TimeSpan.TicksPerSecond;
+                if ((Stop.Ticks - Start.Ticks)%TimeSpan.TicksPerSecond > 0)
                     seconds++;
             }
             else
             {
-                seconds = (DateTime.Now.Ticks - Start.Ticks) / TimeSpan.TicksPerSecond;
-                if ((DateTime.Now.Ticks - Start.Ticks) % TimeSpan.TicksPerSecond > 0)
+                seconds = (DateTime.Now.Ticks - Start.Ticks)/TimeSpan.TicksPerSecond;
+                if ((DateTime.Now.Ticks - Start.Ticks)%TimeSpan.TicksPerSecond > 0)
                     seconds++;
             }
             return seconds;
         }
-
-        /*
-        public long DurationInMinutes()
-        {
-            long minutes = 0;
-            if (Stop.Year != 1)
-            {
-                minutes = (Stop.Ticks - Start.Ticks) / TimeSpan.TicksPerMinute;
-            }
-            else
-            {
-                minutes = (DateTime.Now.Ticks - Start.Ticks) / TimeSpan.TicksPerMinute;
-            }
-            return minutes;
-        }
-        */
     }
 }
