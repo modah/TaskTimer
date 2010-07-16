3f15f977fb1eeddd480ac200aa3304b97b70a323
Jan-Peter Hashagen <jan.hashagen@msu-solutions.de>
2010-07-16 17:15:09 +0200

    Fixing build.

--- Task Timer/Task.cs
+++ Task Timer/Task.cs
@@ -1,16 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Collections;
 
 namespace Task_Timer
 {
-    class Task : IComparable
+    internal class Task : IComparable
     {
-
-        private long timeDifference = 0; //in seconds
-
         public Task(string taskName)
         {
             Taskname = taskName;
@@ -21,104 +16,97 @@ namespace Task_Timer
 
         public string Taskname { get; set; }
 
-        public bool IsActive {get; private set;}
+        public bool IsActive { get; private set; }
 
-        public long TimeDifference
-        {
-            get { return timeDifference; }
-        }
+        public long TimeDifference { get; private set; }
+
         public long TimeDifferenceInMinutes
         {
             get
             {
-                long minutes = timeDifference / 60;
-                if (timeDifference % 60 > 0)
+                long minutes = TimeDifference/60;
+                if (TimeDifference%60 > 0)
                     minutes++;
                 return minutes;
             }
         }
 
-        public override bool Equals(object obj)
-        {
-            return this.Taskname.Equals(((Task)obj).Taskname);
-        }
+        #region IComparable Members
 
         public int CompareTo(object obj)
         {
             return 0;
         }
 
+        #endregion
+
+        public override bool Equals(object obj)
+        {
+            return Taskname.Equals(((Task) obj).Taskname);
+        }
+
         public void Start()
         {
             TimeEntries.Add(new TimeEntry());
             TimeEntries[TimeEntries.Count - 1].Start = DateTime.Now;
-            isActive = true;
+            IsActive = true;
         }
 
         public void Stop()
         {
             TimeEntries[TimeEntries.Count - 1].Stop = DateTime.Now;
-            isActive = false;
+            IsActive = false;
         }
 
         public long DurationInSeconds()
         {
-            long seconds = 0;
-            foreach (var timeEntry in TimeEntries)
-            {
-                seconds += timeEntry.DurationInSeconds();
-            }
-            long _duration = seconds + timeDifference;
-            if (_duration > 0)
-                return _duration;
-            else
-                return 0;
+            long seconds = TimeEntries.Sum(timeEntry => timeEntry.DurationInSeconds());
+            long duration = seconds + TimeDifference;
+            if (duration > 0)
+                return duration;
+            return 0;
         }
 
         public long DurationInMinutes()
         {
-            long seconds = this.DurationInSeconds();
-            long minutes = seconds / 60;
-            if (seconds % 60 > 0)
+            long seconds = DurationInSeconds();
+            long minutes = seconds/60;
+            if (seconds%60 > 0)
                 minutes++;
             return minutes;
         }
 
         public long DurationInSeconds(DateTime date)
         {
-            long seconds = 0;
-            foreach (var timeEntry in TimeEntries)
-            {
-                if (timeEntry.Start.Date.Equals(date.Date) ||
-                    timeEntry.Stop.Date.Equals(date.Date))
-                    seconds += timeEntry.DurationInSeconds();
-            }
-            long _duration = seconds + timeDifference;
-            if (_duration > 0)
-                return _duration;
-            else
-                return 0;
+            long seconds =
+                TimeEntries
+                    .Where(timeEntry => timeEntry.Start.Date.Equals(date.Date) || timeEntry.Stop.Date.Equals(date.Date))
+                    .Sum(timeEntry => timeEntry.DurationInSeconds());
+            long duration = seconds + TimeDifference;
+            if (duration > 0)
+                return duration;
+            return 0;
         }
 
         public long DurationInMinutes(DateTime date)
         {
-            long seconds = this.DurationInSeconds(date);
-            long minutes = seconds / 60;
-            if (seconds % 60 > 0)
+            long seconds = DurationInSeconds(date);
+            long minutes = seconds/60;
+            if (seconds%60 > 0)
                 minutes++;
             return minutes;
         }
 
-        public void AddSeconds(long _seconds)
+        public void AddSeconds(long seconds)
         {
             //TODO
             //if (DurationInSeconds()+_seconds<0
-            timeDifference += _seconds;
+            TimeDifference += seconds;
         }
 
         public void AddMinutes(long minutes)
         {
-            AddSeconds(minutes * 60);
+            AddSeconds(minutes*60);
         }
     }
 }
